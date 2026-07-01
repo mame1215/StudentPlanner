081942b52abf2e02bc2cb1d439310179bb858564
Mame <hazekanji@gmail.com>
2026-07-01 21:36:34 +0900

    Add task due dates and sorting

--- docs/app.js
+++ docs/app.js
@@ -14,24 +14,66 @@ let tasks = [
 const taskList = document.getElementById("taskList");
 const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
+const dueInput = document.getElementById("dueInput")
 
 //タスクのリストを更新
 function renderTasks(taskArray) {
+    
+    //期限順に並び替え
+    const sortedTasks = [...taskArray];
+    sortedTasks.sort(function (a, b) {
+
+        if (!a.due && !b.due) {
+            return 0;
+        }
+
+        if (!a.due) {
+            return 1;
+        }
+
+        if (!b.due) {
+            return -1;
+        }
+
+        return new Date(a.due) - new Date(b.due);
+    });
+
     taskList.innerHTML = "";
 
-    for (const task of taskArray) {
+    for (const task of sortedTasks) {
         const li = document.createElement("li");
 
-        if (tasks.completed) {
+        if (task.completed) {
             li.classList.add("completed");
         }
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        const span = document.createElement("span");
+        const content = document.createElement("div");
+
+        const title = document.createElement("div")
+        const due = document.createElement("div");
+
+        content.classList.add("task-content");
+        title.classList.add("task-title");
+        due.classList.add("task-due");
+
         const button = document.createElement("button");
 
-        span.textContent = task.title;
+        title.textContent = task.title;
+        if (task.due) {
+            const date = new Date(task.due);
+
+            const hour = String(date.getHours()).padStart(2, "0");
+            const minute = String(date.getMinutes()).padStart(2, "0");
+
+            due.textContent =
+            "📅 "+ date.getFullYear()
+            + "/"+ (date.getMonth() + 1)
+            + "/" + date.getDate()
+            + " " + hour
+            + ":" + minute;
+        }
         button.textContent = "🗑"
         checkbox.checked = task.completed;
 
@@ -46,19 +88,23 @@ function renderTasks(taskArray) {
             renderTasks(tasks);
         });
 
+        content.appendChild(title);
+        content.appendChild(due);
+
         li.appendChild(checkbox);
-        li.appendChild(span);
+        li.appendChild(content);
         li.appendChild(button);
         taskList.appendChild(li);
     }
 }
 
 //タスクに追加
-function addTask(taskName) {
+function addTask(taskName, deadline) {
     tasks.push({
-        id: tasks.length + 1,
+        id: Date.now(),
         title: taskName,
-        completed: false
+        completed: false,
+        due: deadline
     });
 
     saveTasks();
@@ -67,11 +113,13 @@ function addTask(taskName) {
 //入力欄から読み取り、タスクに追加し、画面を更新
 function submitTask() {
     const newTask = taskInput.value.trim();
+    const deadline = dueInput.value;
 
     if(newTask !== ""){
-        addTask(newTask);
+        addTask(newTask,deadline);
         renderTasks(tasks);
         taskInput.value = "";
+        dueInput.value = "";
     }
 }
 
